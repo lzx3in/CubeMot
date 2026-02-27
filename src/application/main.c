98c2d639a87c99c9eba4a28aabc55481b2db3ed7
lzx3in <lzx3in@qunle.cc>
2026-02-27 20:17:43 +0800

    refactor(main): replace while(1) with for(;;) infinite loops

--- src/application/main.c
+++ src/application/main.c
@@ -11,13 +11,13 @@ int main(void)
 #if CONFIG_BOARD_HAS_LED1
     led_t led1;
     if (led_init(&led1, BOARD_LED_1) == LED_OK) {
-        while (1) {
+        for (;;) {
             led_toggle(&led1);
             board_delay_ms(500);
         }
     }
 #else
-    while (1) {
+    for (;;) {
     }
 #endif
 }

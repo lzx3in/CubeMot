d5add214bfdf00280459f4ae7ae0d09b91c705ed
lzx3in <lzx3in@qunle.cc>
2026-02-27 21:02:05 +0800

    feat(rtos): migrate main loop to FreeRTOS task
    Signed-off-by: lzx3in <lzx3in@qunle.cc>

--- src/application/main.c
+++ src/application/main.c
@@ -3,22 +3,37 @@
 #include "boards/delay.h"
 #include "boards/init.h"
 #include "cubemot_config.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
-int main(void)
+static void vTaskLED(void *pvParameters)
 {
-    board_init();
+    (void)pvParameters;
 
 #if CONFIG_BOARD_HAS_LED1
     led_t led1;
     if (led_init(&led1, BOARD_LED_1) == LED_OK) {
         for (;;) {
             led_toggle(&led1);
-            board_delay_ms(500);
+            vTaskDelay(pdMS_TO_TICKS(500));
         }
     }
 #else
     for (;;) {
-        board_delay_ms(1000);
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 #endif
+}
+
+int main(void)
+{
+    board_init();
+
+    xTaskCreate(vTaskLED, "LED", 256, NULL, 1, NULL);
+
+    vTaskStartScheduler();
+
+    // Should never reach here
+    for (;;) {
+    }
 }

7838e0ad22422c339c5b14e85c9561e00c2521ff
lzx3in <lzx3in@qunle.cc>
2026-03-05 07:08:25 +0800

    feat: add FreeRTOS mocks for testing
    Signed-off-by: lzx3in <lzx3in@qunle.cc>

---
+++ tests/mocks/freertos/FreeRTOS.h
@@ -0,0 +1,6 @@
+#ifndef FREERTOS_H
+#define FREERTOS_H
+
+#include "freertos_stubs.h"
+
+#endif // FREERTOS_H
---
+++ tests/mocks/freertos/freertos_stubs.h
@@ -0,0 +1,29 @@
+#ifndef FREERTOS_STUBS_H
+#define FREERTOS_STUBS_H
+
+#include <stdint.h>
+#include <time.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define pdMS_TO_TICKS(xTimeInMs) (xTimeInMs)
+#define portTICK_PERIOD_MS 1
+
+typedef uint32_t TickType_t;
+
+// vTaskDelay stub using nanosleep
+static inline void vTaskDelay(TickType_t xTicksToDelay)
+{
+    struct timespec ts;
+    ts.tv_sec = xTicksToDelay / 1000;
+    ts.tv_nsec = (xTicksToDelay % 1000) * 1000000;
+    nanosleep(&ts, NULL);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // FREERTOS_STUBS_H

3388303d1a9fdc1e4f64944b9223144a30563c9e
lzx3in <lzx3in@qunle.cc>
2026-06-06 13:51:27 +0000

    fix(common): add device enum validation helpers
    Add cubemot_device_led_is_valid() and cubemot_device_button_is_valid()
    inline functions for bounds checking device IDs.

--- src/common/include/common_device.h
+++ src/common/include/common_device.h
@@ -21,6 +21,16 @@ typedef enum {
     CUBEMOT_DEVICE_BUTTON_COUNT,
 } cubemot_device_button;
 
+static inline bool cubemot_device_led_is_valid(cubemot_device_led led_id)
+{
+    return (led_id >= CUBEMOT_DEVICE_LED_0 && led_id < CUBEMOT_DEVICE_LED_COUNT);
+}
+
+static inline bool cubemot_device_button_is_valid(cubemot_device_button button_id)
+{
+    return (button_id >= CUBEMOT_DEVICE_BUTTON_0 && button_id < CUBEMOT_DEVICE_BUTTON_COUNT);
+}
+
 #ifdef __cplusplus
 }
 #endif
